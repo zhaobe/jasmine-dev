e83ad07b5b96806deb0ed999c0995a1ac271a96a
zhaobe <zhaoben777@gmail.com>
2017-04-09 22:38:28 -0700

    Adding subtract, multiply, and divide functions.

--- tests/jasmine-standalone-2.5.2/spec/CalculatorSpec.js
+++ tests/jasmine-standalone-2.5.2/spec/CalculatorSpec.js
@@ -2,8 +2,20 @@ describe("calculator", function () {
     var num1 = Math.floor((Math.random() * 100) + 1),
         num2 = Math.floor((Math.random() * 100) + 1);
     
-    it("should add numbers", function() {
+    it("should add " + num1 + " and " + num2 + " to equal " + (num1 + num2), function() {
         expect(Calculator.add(num1, num2)).toBe(num1 + num2);
+    });
+    
+    it("should subtract " + num1 + " and " + num2 + " to equal " + (num1 - num2), function() {
+        expect(Calculator.sub(num1, num2)).toBe(num1 - num2);
+    });
+    
+    it("should multiply " + num1 + " and " + num2 + " to equal " + (num1 * num2), function() {
+        expect(Calculator.mult(num1, num2)).toBe(num1 * num2);
+    });
+    
+    it("should divide " + num1 + " and " + num2 + " to equal " + (num1 / num2), function() {
+        expect(Calculator.div(num1, num2)).toBe(num1 / num2);
     });
     
 });
--- tests/jasmine-standalone-2.5.2/src/Calculator.js
+++ tests/jasmine-standalone-2.5.2/src/Calculator.js
@@ -3,4 +3,13 @@ var Calculator = {
     add: function (num1, num2) {
         return num1 + num2;
     },
+    sub: function (num1, num2) {
+        return num1 - num2;
+    },
+    mult: function (num1, num2) {
+        return num1 * num2;
+    },
+    div: function (num1, num2) {
+        return num1 / num2;
+    }
 };
